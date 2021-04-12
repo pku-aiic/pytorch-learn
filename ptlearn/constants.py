e938b00af03d45d5975fd43c5656ada732fb5810
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2021-04-12 20:00:05 +0800

    ✨Introduced `constants`

---
+++ ptlearn/constants.py
@@ -0,0 +1,11 @@
+INFO_PREFIX = "> [info] "
+ERROR_PREFIX = "> [error] "
+WARNING_PREFIX = "> [warning] "
+
+LOSS_KEY = "loss"
+INPUT_KEY = "input"
+PREDICTIONS_KEY = "predictions"
+LABEL_KEY = "labels"
+
+PT_PREFIX = "model_"
+SCORES_FILE = "scores.json"
